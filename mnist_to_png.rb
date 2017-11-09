acc5c7df1cc23d11b93d8e1f9f64a456fe83c762
Patrick Tangphao <patrick.tangphao@gmail.com>
2017-11-09 02:13:57 -0800

    Add pseudocode for converter

---
+++ mnist_to_png.rb
@@ -0,0 +1,16 @@
+require 'zlib'
+require 'chunky_png'
+
+# initialize using training set
+
+# get_data_and labels
+  # load data
+  # create 2 arrays
+  # unpack data
+  # return an array with 1. image data and 2. value data
+
+
+# grab data from get_data_and_labels
+
+# method that creates an image from the image format
+# utilize that method
