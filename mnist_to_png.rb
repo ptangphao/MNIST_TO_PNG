93fba51b229a37fa6fc90b104876f36f78257fe0
Patrick Tangphao <patrick.tangphao@gmail.com>
2017-11-11 16:05:50 -0800

    remove pseudocode

--- mnist_to_png.rb
+++ mnist_to_png.rb
@@ -1,20 +1,6 @@
 require 'zlib'
 require 'chunky_png'
 
-# initialize using training set
-
-# get_data_and labels
-  # load data
-  # create 2 arrays
-  # unpack data
-  # return an array with 1. image data and 2. value data
-
-
-# grab data from get_data_and_labels
-
-# method that creates an image from the image format
-# utilize that method
-
 class MnistPNGGenerator
   WHITE = ChunkyPNG::Color.from_hex('#FFFFFF')
 
